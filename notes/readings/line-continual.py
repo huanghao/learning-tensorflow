690ef7fb53483ae4e33b619fd328752dad7a2516
Huang Hao <huang1hao@gmail.com>
2017-06-05 19:47:37 +0800

    update

--- notes/readings/line-continual.py
+++ notes/readings/line-continual.py
@@ -1,6 +1,26 @@
 import sys
+import argparse
+import subprocess
+
+
+def parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument('-c', '--copy', action='store_true')
+    return p.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    input_ = sys.stdin.read()
+    text = input_.replace('- ', '').replace('\r', '\n'*2)
+
+    if args.copy:
+        p = subprocess.Popen(['pbcopy'], stdin=subprocess.PIPE, close_fds=True)
+        p.communicate(input=text)
+    else:
+        print text
 
 
 if __name__ == '__main__':
-    for line in sys.stdin:
-        print line.replace('-\r', '').replace('\r', ' ')
+    main()
