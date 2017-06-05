d54c10f8276ecd365f5a6651c7d2ce99f39ba619
Huang Hao <huang1hao@gmail.com>
2017-06-05 19:19:35 +0800

    update gan

--- notes/readings/line-continual.py
+++ notes/readings/line-continual.py
@@ -3,4 +3,4 @@ import sys
 
 if __name__ == '__main__':
     for line in sys.stdin:
-        print line.replace('- ', '').replace('\r', '\n').replace('\n', '\n'*4)
+        print line.replace('-\r', '').replace('\r', ' ')
