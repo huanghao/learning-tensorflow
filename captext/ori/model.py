5d3a5bbddee8c83998092a08c56f141296b95094
KSword <511403401@qq.com>
2017-06-19 14:08:52 +0800

    Return 3 conv layer, change train dropout 0.75 => 0.5

--- captext/ori/model.py
+++ captext/ori/model.py
@@ -18,7 +18,7 @@ def crack_captcha_cnn(w_alpha=0.01, b_alpha=0.1):
     # w_d1_alpha = np.sqrt(2.0/(8*32*64))
     # out_alpha = np.sqrt(2.0/1024)
 
-    # 4 conv layer
+    # 3 conv layer
     w_c1 = tf.Variable(w_alpha * tf.random_normal([3, 3, 1, 32]))
     b_c1 = tf.Variable(b_alpha * tf.random_normal([32]))
     conv1 = tf.nn.relu(tf.nn.bias_add(
@@ -43,19 +43,19 @@ def crack_captcha_cnn(w_alpha=0.01, b_alpha=0.1):
                            strides=[1, 2, 2, 1], padding='SAME')
     conv3 = tf.nn.dropout(conv3, keep_prob)
 
-    w_c4 = tf.Variable(w_alpha * tf.random_normal([3, 3, 64, 128]))
-    b_c4 = tf.Variable(b_alpha * tf.random_normal([128]))
-    conv4 = tf.nn.relu(tf.nn.bias_add(
-        tf.nn.conv2d(conv3, w_c4, strides=[1, 1, 1, 1], padding='SAME'), b_c4))
-    conv4 = tf.nn.max_pool(conv4, ksize=[1, 2, 2, 1],
-                           strides=[1, 2, 2, 1], padding='SAME')
-    conv4 = tf.nn.dropout(conv4, keep_prob)
+    #w_c4 = tf.Variable(w_alpha * tf.random_normal([3, 3, 64, 128]))
+    #b_c4 = tf.Variable(b_alpha * tf.random_normal([128]))
+    #conv4 = tf.nn.relu(tf.nn.bias_add(
+    #    tf.nn.conv2d(conv3, w_c4, strides=[1, 1, 1, 1], padding='SAME'), b_c4))
+    #conv4 = tf.nn.max_pool(conv4, ksize=[1, 2, 2, 1],
+    #                       strides=[1, 2, 2, 1], padding='SAME')
+    #conv4 = tf.nn.dropout(conv4, keep_prob)
 
     # Fully connected layer
     _, a, b, c = conv4.get_shape().as_list()
     w_d = tf.Variable(w_alpha * tf.random_normal([a * b * c, 1024]))
     b_d = tf.Variable(b_alpha * tf.random_normal([1024]))
-    dense = tf.reshape(conv4, [-1, w_d.get_shape().as_list()[0]])
+    dense = tf.reshape(conv3, [-1, w_d.get_shape().as_list()[0]])
     dense = tf.nn.relu(tf.add(tf.matmul(dense, w_d), b_d))
     dense = tf.nn.dropout(dense, keep_prob)
 
